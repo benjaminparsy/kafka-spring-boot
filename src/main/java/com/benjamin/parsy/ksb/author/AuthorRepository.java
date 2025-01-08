benjaminparsy/benjaminparsy-java-21-virtual-threads-spring-boot-

Preparation of all business logic and the REST layer

src/main/java/com/benjamin/parsy/vtsb/author/AuthorRepository.java
@@ -1,0 +1,6 @@
+package com.benjamin.parsy.vtsb.author;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AuthorRepository extends JpaRepository<Author, Long> {
+}

src/main/java/com/benjamin/parsy/vtsb/author/dto/AuthorResponseDto.java
@@ -1,0 +1,24 @@
+package com.benjamin.parsy.vtsb.author.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class AuthorResponseDto {
+
+    @JsonProperty
+    private Long id;
+
+    @JsonProperty
+    private String firstname;
+
+    @JsonProperty
+    private String lastname;
+
+}

benjaminparsy/kafka-spring-boot

src/main/java/com/benjamin/parsy/ksb/author/AuthorRepository.java
@@ -1,0 +1,6 @@
+package com.benjamin.parsy.ksb.author;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AuthorRepository extends JpaRepository<Author, Long> {
+}

src/main/java/com/benjamin/parsy/ksb/author/dto/AuthorResponseDto.java
@@ -1,0 +1,24 @@
+package com.benjamin.parsy.ksb.author.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class AuthorResponseDto {
+
+    @JsonProperty
+    private Long id;
+
+    @JsonProperty
+    private String firstname;
+
+    @JsonProperty
+    private String lastname;
+
+}